df5963b960dc0a0cdc6b5df81d504a82c378c92b
joshuaning <joshning@umich.edu>
2025-03-04 20:54:25 -0500

    clean sentences

---
+++ readme.md
@@ -0,0 +1,17 @@
+# JSCC for Semantic Text Transmission: Comm with Built-in Translation
+
+Semantic communication such as deep JSCC has been proven to work well in low SNR enviornment. 
+Compared to traditional source coding and channel coding, JSCC does not suffer from the water
+fall effect. However, in deep JSCC for text communication, the sentences recieved often suffer 
+from semantic distortion, which result in the sentence recieved not exactly matching with the 
+sentences transmitted.
+
+In realistic communication senarios, most users want to receive exact text data transmitted. 
+We decided to embrace this semantic distortion by utilizing deep JSCC for translation, since
+semantic distortion is expected and tolerable in translation applications.
+
+Our deep JSCC is modular, allowing fast re-training for switching languages and channel models.
+This modularity can also be used for simultanous translation to multiple languages  
+
+
+Authors: Joshua Ning, Jason Ning
--- src/preprocess.py
+++ src/preprocess.py
@@ -8,22 +8,35 @@ import json
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--input-data-dir', default='C:/Users/Joshua Ning/Documents/Dataset/europarl/txt/da', type=str)
+parser.add_argument('--output-train-dir', default='dataset/da/train_data_da.pkl', type=str)
+parser.add_argument('--output-test-dir', default='dataset/da/test_data_da.pkl', type=str)
+parser.add_argument('--output-vocab', default='dataset/da/vocab_da.json', type=str)
+
 
 
 def clean_string(s):
     '''
     warning: current implementation only works on words sepearted by space
     1. remove XML-tages from string
-    2. add white space before and after special characters
-    3. add white space before and after each number
-    4. make sure only one white space seperates each token
-    5. turn all letters to lower case for normalization
+    2. remove list formating
+    3. add white space before and after special characters
+    4. add white space before and after each number
+    5. make sure only one white space seperates each token
+    6. turn all letters to lower case for normalization
     '''
     # remove XML tags
     s = remove_tags(s)
 
-    # add white space before and after !@#$%^&*()_+:"":;,./?<>-
-    s = re.sub(r'([!@#$%^&*()_+"":;,./?<>-])', r' \1 ', s)
+    # remove list  formating (leading number and dot)
+    s = re.sub(r'^\d+\.\s*', '', s)
+
+    # remove content within () and any ()
+    s = re.sub(r'\([^)]*\)', '', s)
+    s = re.sub(r'[()]', '', s)
+
+    # add white space before and after !@#$%^&*_+:"":;,./?<>-
+    s = re.sub(r'([!@#$%^&*_+"":;,./?<>-])', r' \1 ', s)
 
     #add white space before each number
     s = re.sub(r'(?<=\d)(?=\d)', r' ', s)
@@ -36,16 +49,48 @@ def clean_string(s):
 
     return s
 
+def trim_data(cleaned, MIN_LENGTH=4, MAX_LENGTH=50):
+    '''
+    ensure each sentence in cleaned is between token length 4-50
+    '''
+    trimed_lines = list()
+    for line in cleaned:
+        length = len(line.split())
+        if length > MIN_LENGTH and length < MAX_LENGTH:
+            trimed_lines.append(line)
+    return trimed_lines
 
-if __name__ == '__main__':
-    file = open("C:\\Users\\Joshua Ning\\Downloads\\DeepSC-master\\europarl\\txt\\da\\ep-11-11-16-001.txt", 'r', encoding='utf8')
+def clean_file(fname):
+    '''
+    preprocess a files
+    '''
+
+    file = open(fname, 'r', encoding='utf8')
     raw_data = file.read()
     sentences = raw_data.strip().split('\n')
-    raw_data_input = [clean_string(data) for data in sentences]
-    print(raw_data_input)
-
-
+    clean_data = [clean_string(data) for data in sentences]
+    clean_data = trim_data(clean_data)
     file.close()
+    return(clean_data)
+
+def clean_folder(folder_name):
+    unique_sentences = set()
+    for file in tqdm(os.listdir(folder_name)):
+        if file.endswith('.txt'): 
+            process_sentences = clean_file(os.path.join(folder_name, file))
+            unique_sentences.update(process_sentences)
+    
+    return list(unique_sentences)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    sentences = clean_folder(args.input_data_dir)
+    print("there are {} unique sentences. \n\n".format(len(sentences)))
+
+    for i in range(10):
+        print(sentences[i])
+
 
 
 
